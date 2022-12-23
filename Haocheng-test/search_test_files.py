edd79c2fa80c28c6102fac8f18c06b63749424d2
hlin863 <zcabhli@ucl.ac.uk>
2022-12-23 11:54:02 +0000

    Update search_test_files.py
    Implemented a method to calculate the number of repositories that I will scrap from the website.

--- Haocheng-test/search_test_files.py
+++ Haocheng-test/search_test_files.py
@@ -7,6 +7,13 @@ repos = [] # initalise a list of repositories to be analysed.
 apache_site = "https://github.com/orgs/apache/repositories" # the URL of the Apache GitHub site.
 
 # get the list of repositories from the Apache GitHub site.
-repos_list = requests.get(apache_site).text
+r = requests.get(apache_site)
 
-print(repos_list)
+# extract the repository names from the list of repositories.
+for line in r.text.splitlines():
+
+    if "href=\"/apache/" in line:
+        repo_name = line.split("/")[2]
+        repos.append(repo_name)
+
+print("There are {} repositories in the Apache GitHub site.".format(len(repos)))
