115f1cf23ccda679ee373e1e1d74f3100a352a31
hlin863 <zcabhli@ucl.ac.uk>
2022-12-23 12:17:50 +0000

    Update test_class_occurrence.py
    I implemented the skeleton code for storing the commit messages in a list for future storage in local files (analysis).
    
    Organized the code in a more presentable format.

--- Haocheng-test/test_class_occurrence.py
+++ Haocheng-test/test_class_occurrence.py
@@ -1,39 +1,22 @@
 # This file experiments with the pydriller library
 
 from pydriller import Repository
+import pandas as pd # import the pandas library to convert the commit messages into a dataframe for storage and analysis.
 import glob
 
 modified_files = 0
 
 repos = ["https://github.com/hlin863/Supervised_Learning_CW2_group_repo", "https://github.com/ishepard/pydriller"]
 
-for commit in Repository(path_to_repo=repos).traverse_commits():
+apache_repos = ['https://github.com/apache/camel-website'] # initialise to list for storing and analysing the Apache repositories.
 
-    print("Project {}, commit {}, date {}".format(
-           commit.project_path, commit.hash, commit.author_date))
+commit_msgs = [] # initialise a list to store the commit messages.
 
-"""
-# traverse all commits in the repository and displays the commit has, the commit message, the author name and the modified files
-for commit in Repository('https://github.com/ishepard/pydriller').traverse_commits():
-    # print(commit.hash)
-    # print(commit.msg)
-    # print(commit.author.name)
+for commit in Repository(apache_repos).traverse_commits():
 
-    commit_file_names = [] # list to track the modified files in each commit.
+    commit_msgs.append(commit.msg) # append the commit message to the list.
 
-    n_test_files = 0 # initialise a number to track the testing files for each commit.
-
-    for file in commit.modified_files:
-        # check if filename contains the word "test"
-        if "test" in file.filename:
-            n_test_files += 1 # increments the number of test files modified during each commit.
-
-        commit_file_names.append(file.filename)
-
-        # print(file.filename, ' has changed')
-        modified_files += 1
-
-    print("The number of test files modified in this commit is: ", n_test_files)
-
-print("Total modified files: ", modified_files)
-"""
+    print(commit.msg)
+    print(commit.hash)
+    print(commit.insertions)
+    print(commit.deletions)
